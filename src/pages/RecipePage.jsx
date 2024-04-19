9d469860212a3596369896c3227efa611c45b692
Zhibin Ren <zhibinren79@gmail.com>
2024-04-18 18:24:32 -0700

    update to recipepage and recipe json for test

--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -25,7 +25,9 @@ const RecipePage = () => {
       <button onClick={goBack} className="back-button">Back</button>
       <div className="recipe-header">
         <img src={recipe.picture} alt={recipe.name} className="recipe-image" />
-        <h1 className="recipe-title-page">{recipe.name}</h1> {/* Title added below the image */}
+        <h1 className="recipe-title-page">{recipe.name}</h1>
+        {/* Displaying the number of servings below the recipe name */}
+        <p className="recipe-servings">{recipe.servings}人份</p>
       </div>
       
       <div className="recipe-body">

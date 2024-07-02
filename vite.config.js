f2c6e73c46eddb384ed90f6c7d793203bfbdd2e9
Zhibin Ren <zhibinren79@gmail.com>
2024-07-02 04:33:09 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/vite-project/',
+  base: '/rensrecipe.com/',
   plugins: [react()]
 })
